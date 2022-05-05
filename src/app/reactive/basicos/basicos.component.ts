e8b78a668ea466e98266edad27c9172e0812ea9f
Javier Jure <jjure13@gmail.com>
2022-05-05 13:13:15 -0300

    Añadiendo forms reactive

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class BasicosComponent implements OnInit {
-
+  miFormulario : FormGroup = new FormGroup({
+    nombre:new FormControl('Javier'),
+    precio:new FormControl(0),
+    existencias:new FormControl(15)
+  })
   constructor() { }
 
   ngOnInit(): void {
